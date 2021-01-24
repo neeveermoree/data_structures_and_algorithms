c2ecc3d21dad4ca60042ba94d6e280483ad50b71
Anton Khanaev <askhanaev@edu.hse.ru>
2021-01-24 18:56:27 +0300

    Circular Linked List is created and displayed

---
+++ Linked List/4.2-circular_linked_list.py
@@ -0,0 +1,50 @@
+class _Node:
+    __slots__ = ('_val', '_next_node')
+    def __init__(self, val, next_node=None):
+        self._val = val
+        self._next_node = next_node
+
+
+class CircularLinkedList:
+    __slots__ = ('_head', '_length')
+    def __init__(self):
+        self._head = None
+        self._length = 0
+    
+    def __len__(self):
+        return self._length
+    
+    def add_last(self, val):
+        node = _Node(val)
+        if not len(self):
+            node._next_node = node
+            self._head = node
+        cur_node = self._head
+        while cur_node._next_node != self._head:
+            cur_node = cur_node._next_node
+        node._next_node = self._head
+        cur_node._next_node = node
+        self._length += 1
+
+    def display(self):
+        if not len(self):
+            print('Circular Linked List is empty')
+            return None
+        cur_node = self._head
+        print(f'{cur_node._val} --> ', end='')
+        while cur_node._next_node != self._head:
+            print(f'{cur_node._next_node._val} --> ', end='')
+            cur_node = cur_node._next_node
+        print()
+
+
+cll = CircularLinkedList()
+cll.display()
+cll.add_last(1)
+print(len(cll))
+cll.display()
+cll.add_last(5)
+print(len(cll))
+cll.display()
+cll.add_last(12)
+cll.display()
