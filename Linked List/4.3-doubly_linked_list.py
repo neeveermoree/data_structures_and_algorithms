2657ed44bda6912e465e66017af758e358b45993
Anton Khanaev <askhanaev@edu.hse.ru>
2021-05-31 22:53:55 +0300

    display and add_last methods are added to DLL

--- Linked List/4.3-doubly_linked_list.py
+++ Linked List/4.3-doubly_linked_list.py
@@ -15,8 +15,38 @@ class DoublyLinkedList:
 
     def __len__(self):
         return self._length
+
+    def display(self):
+        if not len(self):
+            print("Doubly Linked List is empty")
+            return None
+        elif len(self) == 1:
+            print(f'{self._head._val} -> ')
+            return None
+        s = f'{self._head._val} -> '
+        cur_node = self._head
+        while cur_node._next_node != self._head:
+            cur_node = cur_node._next_node
+            s += f'{cur_node._val} -> '
+        print(s)
     
+    def add_last(self, val):
+        node = _Node(val)
+        if not len(self):
+            self._head = node
+            self._tail = self._head
+        self._tail._next_node = node
+        self._head._previous_node = node
+        node._previous_node = self._tail
+        node._next_node = self._head
+        self._length += 1
+
 
 node = _Node(0)
 dll = DoublyLinkedList()
 print(len(dll))
+dll.display()
+dll.add_last(1)
+dll.display()
+dll.add_last(2)
+dll.display()
