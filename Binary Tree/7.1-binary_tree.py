bfdf132e688b9cbb291458daca26c9cb440f9c10
Anton Khanaev <askhanaev@edu.hse.ru>
2021-06-19 13:32:52 +0300

    Preorder traversal is implemented

--- Binary Tree/7.1-binary_tree.py
+++ Binary Tree/7.1-binary_tree.py
@@ -10,3 +10,16 @@ class BinaryTree:
     __slots__ = '_root'
     def __init__(self, root=None):
         self._root = root
+
+    def preorder_traversal(self, node):
+        if node is not None:
+            print(node._val, end=' ')
+            self.preorder_traversal(node._left)
+            self.preorder_traversal(node._right)
+            
+
+left_node = _Node(2)
+right_node = _Node(3)
+root_node = _Node(1, left_node, right_node)
+bt = BinaryTree(root_node)
+bt.preorder_traversal(bt._root)
