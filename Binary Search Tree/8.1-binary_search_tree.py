8b6e376af2ba4173a9cdf158d5046876b832205e
Anton Khanaev <askhanaev@edu.hse.ru>
2021-07-23 22:28:23 +0300

    Iterative Insertion is implemented

--- Binary Search Tree/8.1-binary_search_tree.py
+++ Binary Search Tree/8.1-binary_search_tree.py
@@ -31,6 +31,32 @@ class BinarySearchTree:
             return self.search_recursive(node._left, val)
         else:
             return self.search_recursive(node._right, val)
+    
+    def inorder_traversal(self, node):
+        if node:
+            self.inorder_traversal(node._left)
+            print(node._val, end='')
+            self.inorder_traversal(node._right)
+
+    def insert_iterative(self, val):
+        new_node = _Node(val)
+        node = self._root
+        if not node:
+            self._root = new_node
+            return
+        while node:
+            if node._val <= val:
+                if node._right:
+                    node = node._right
+                else:
+                    node._right = new_node
+                    return
+            else:
+                if node._left:
+                    node = node._left
+                else:
+                    node._left = new_node
+                    return
 
 
 """
@@ -66,3 +92,20 @@ print(bt1.search_recursive(bt1._root, 1))
 print(bt1.search_recursive(bt1._root, 5))
 print(bt1.search_recursive(bt1._root, 8))
 print(bt1.search_recursive(bt1._root, 2))
+
+print('\nInorder Traversal')
+bt1.inorder_traversal(bt1._root)
+print()
+
+print('\nIterative Insertion')
+bt1.insert_iterative(10)
+bt1.inorder_traversal(bt1._root)
+print()
+bt1.insert_iterative(7)
+bt1.inorder_traversal(bt1._root)
+print()
+bt1.insert_iterative(0)
+bt1.inorder_traversal(bt1._root)
+print()
+bt1.insert_iterative(2)
+bt1.inorder_traversal(bt1._root)
